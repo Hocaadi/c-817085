7e082ad1d5768fbb8ec1e95defc6154c66872826
Hocaadi <aadarsh@hoc.com>
2025-02-23 21:42:51 +0530

    sequence 1

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
   server: {
     port: 8080
   }
